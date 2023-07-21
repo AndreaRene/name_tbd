736df1bc477f3b3116b583a4ce73327315cbeef9
AndreaRene <andrea@interhact.net>
2023-07-21 10:49:16 -0500

    add front end query variables for chores

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_CHORES = gql`
+query getChores {
+    chores{
+      _id
+      choreTitle
+      choreText
+    }
+  }
+`;
+
+export const QUERY_SINGLE_CHORE = gql`
+  query getSingleChore($choreId: ID!) {
+    chore(choreId: $choreId){
+      _id
+      choreTitle
+      choreText
+    }
+  }
+`;

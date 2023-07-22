aa35041f3c01eb82419ee62b4bc5a7445bd631b3
AndreaRene <andrea@interhact.net>
2023-07-22 15:19:17 -0500

    work on chore type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,8 +2,25 @@ const typeDefs = `
   type Chore {
     _id: ID
     choreTitle: String!
-    choreText: String!
-    choreComplete: Boolean
+    choreText: String
+    choreIsMultiple: Boolean
+    choreDueDate: [String]
+    choreDueTime: [String]
+    choreAmount: Int
+    choreIsComplete: Boolean
+    choreCompleteDates: [String]
+    choreReward: Reward
+    choreCons: Consequence
+  }
+
+  type Reward {
+    rewardTitle: String!
+    rewardText: String
+    rewardCount: Int
+  }
+
+  type Consequence {
+    consTitle: String!
   }
 
   type Query {

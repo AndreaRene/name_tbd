992178344ae0341337b0bb0bb75ee3f84c683d36
AndreaRene <andrea@interhact.net>
2023-07-24 13:12:43 -0500

    add child model

--- server/models/Child.js
+++ server/models/Child.js
@@ -2,9 +2,35 @@ const { Schema, model } = require('mongoose');
 
 
 const childSchema = new Schema({
-
-
-    });
+    childUsername: {
+        type: String,
+        require: true,
+        minlength: 3,
+        maxlength: 20,
+        trim: true,
+        match: /[a-z]|[A-Z]/,
+    },
+    childSibling: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Child'
+    }],
+    childParent: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Parent'
+    }],
+    childChore: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Chore'
+    }],
+    childReward: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Reward'
+    }],
+    childCons: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Consequence'
+    }],
+});
 
 const Child = model('Child', childSchema);
 

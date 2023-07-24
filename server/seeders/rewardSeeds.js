bbe842b6bbb091e0c91db05f961e0bdf7ee7b9a2
AndreaRene <andrea@interhact.net>
2023-07-24 14:33:13 -0500

    completed reward seeds

---
+++ server/seeders/rewardSeeds.js
@@ -0,0 +1,27 @@
+[
+    {
+        "rewardTitle" : "RewardTitle1",
+        "rewardText" : "RewardText1",
+        "rewardCost" : 1,
+        "rewardCount" : 1,
+        "rewardMaxCount" : 1,
+        "rewardIsSpent": false,
+        "rewardExpyDate" : "",
+    },{
+        "rewardTitle" : "RewardTitle2",
+        "rewardText" : "RewardText2",
+        "rewardCost" : 2,
+        "rewardCount" : 2,
+        "rewardMaxCount" : 2,
+        "rewardIsSpent": true,
+        "rewardExpyDate" : "",
+    },{
+        "rewardTitle" : "RewardTitle3",
+        "rewardText" : "RewardText3",
+        "rewardCost" : 3,
+        "rewardCount" : 3,
+        "rewardMaxCount" : 3,
+        "rewardIsSpent": false,
+        "rewardExpyDate" : "",
+    }
+]
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,17 @@
 const db = require('../config/connection');
 const cleanDB = require('./cleanDB');
 
-const { Parent, Child, Chore,  } = require('../models');
+const {
+  Parent,
+  Child,
+  Chore,
+  Reward
+} = require('../models');
 
 const parentSeeds = require('./parentSeeds.json');
 const childSeeds = require('./childSeeds.json');
 const choreSeeds = require('./choreSeeds.json');
+const rewardSeeds = require('./rewardSeeds.json');
 
 db.once('open', async () => {
   try {
@@ -13,10 +19,12 @@ db.once('open', async () => {
     await cleanDB('Parent', 'parents');
     await cleanDB('Child', 'childs');
     await cleanDB('Chore', 'chores');
+    await cleanDB('Reward', 'rewards');
 
     await Parent.create(parentSeeds);
     await Child.create(childSeeds);
     await Chore.create(choreSeeds);
+    await Reward.create(rewardSeeds);
     
   } catch (err) {
     console.error(err);

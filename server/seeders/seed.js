24245941db8b380b90069455f7b9c995ede1af92
AndreaRene <andrea@interhact.net>
2023-07-24 14:24:44 -0500

    added path to child seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,23 @@
 const db = require('../config/connection');
 const cleanDB = require('./cleanDB');
 
-const { Parent, Chore,  } = require('../models');
+const { Parent, Child, Chore,  } = require('../models');
 
 const parentSeeds = require('./parentSeeds.json');
+const childSeeds = require('./childSeeds.json');
 const choreSeeds = require('./choreSeeds.json');
 
 db.once('open', async () => {
   try {
+
     await cleanDB('Parent', 'parents');
+    await cleanDB('Child', 'childs');
     await cleanDB('Chore', 'chores');
+
     await Parent.create(parentSeeds);
+    await Child.create(childSeeds);
     await Chore.create(choreSeeds);
+    
   } catch (err) {
     console.error(err);
     process.exit(1);

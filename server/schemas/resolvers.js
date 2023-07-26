407c540f5640a6d6a3375e2e7839738e10e3a98e
AndreaRene <andrea@interhact.net>
2023-07-26 11:32:25 -0500

    stripped down backend. mutation adds the object but the data return in the sandbox is null

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,49 +1,21 @@
 const {
-    Chore,
-    Parent,
-    Child,
-    Reward,
-    Consequence       
+    Parent      
 } = require('../models');
 
 const resolvers = {
     Query: {
-        chores: async () => {
-            return Chore.find();
-        },
-        oneChore: async (parent, { choreId }) => {
-            return Chore.findOne({ _id: choreId });
-        },
+
         parents: async () => {
             return Parent.find()
         },
-        oneParent: async (parent, { parentId }) => {
-            return Parent.findOne({_id: parentId})
-        },
-        childs: async () => {
-            return Child.find()
-        },
-        oneChild: async (parent, { childId }) => {
-            return Child.findOne({_id: childId})
-        },
-        rewards: async () => {
-            return Reward.find()
-        },
-        oneReward: async (parent, { rewardId }) => {
-            return Reward.findOne({_id: rewardId})
-        },
-        consequences: async () => {
-            return Consequence.find()
-        },
-        oneConsequence: async (parent, { consId }) => {
-            return Consequence.findOne({_id: consId})
-        }
+
     },
 
     Mutation: {
   addParent: async (parent, { parentUsername, parentEmail, parentPhone }) => {
     try {
-      const newParent = await Parent.create({ parentUsername, parentEmail, parentPhone });
+        const newParent = await Parent.create({ parentUsername, parentEmail, parentPhone });
+        console.log(newParent);
       return { newParent };
     } catch (error) {
       console.error("Error while adding parent:", error);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,104 +3,22 @@ const typeDefs = `
     type Parent {
       _id: ID
       parentUsername: String
-      parentEmail: String!
+      parentEmail: String
       parentPhone: String
     }
-
-    type Child{
-      _id: ID
-      childUsername: String!
-      childParent: [Parent]!
-      childSibling: [Child]
-      childChore: [Chore]
-      childReward: [Reward]
-      childCons: [Consequence]
-    }
-
-    type Chore {
-    _id: ID
-    choreTitle: String!
-    choreText: String
-    choreIsMultiple: Boolean
-    choreDueDate: [String]
-    choreDueTime: [String]
-    choreAmount: Int
-    choreIsComplete: Boolean
-    choreCompleteDates: [String]
-    choreReward: [Reward]
-    chorePoints: Int
-    choreCons: [Consequence]
-  }
-
-  type Reward {
-    _id: ID
-    rewardTitle: String!
-    rewardText: String
-    rewardCost: Int
-    rewardCount: Int
-    rewardMaxCount: Int
-    rewardIsSpent: Boolean
-    rewardExpyDate: String
-  }
-
-  type Consequence {
-    _id: ID
-    consTitle: String!
-    consText: String
-    ConsCount: Int
-    consCost: Int
-    consIsSpent: Boolean
-  }
 
   input ParentInput{
     parentUsername: String
-      parentEmail: String!
+      parentEmail: String
       parentPhone: String
   }
 
-  input ChildInput{
-    childUsername: String!
-  }
-
-  input ChoreInput {
-    choreTitle: String!
-    choreText: String
-    choreIsMultiple: Boolean
-    choreDueDate: [String]
-    choreDueTime: [String]
-    choreReward: [String]
-    chorePoints: Int
-    choreCons: [String]
-  }
-
-  input RewardInput {
-    rewardTitle: String!
-    rewardText: String
-    rewardCost: Int
-    rewardExpyDate: String
-  }
-
-  input ConsequenceInput {
-    consTitle: String!
-    consText: String
-    consCost: Int
-  }
-
   type Query {
-    chores: [Chore]
-    oneChore(choreId: ID!): Chore
     parents: [Parent]
-    oneParent(parentId: ID!): Parent
-    childs: [Child]
-    oneChild(childId: ID!): Child
-    rewards: [Reward]
-    oneReward(rewardId: ID!): Reward
-    consequences: [Consequence]
-    oneConsequence(consId: ID!): Consequence
   }
   
   type Mutation {
-    addParent(parentUsername: String, parentEmail: String!, parentPhone: String): Parent
+    addParent(parentUsername: String, parentEmail: String, parentPhone: String): Parent
   }`;
 
 
